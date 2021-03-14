9d72c4a16b35e54d61b00844e5e60cc27f6bbdf6
Chris Smith <chris@cgsmith.net>
2021-03-14 13:25:25 -0500

    adding filtering and custom message

--- Simpleplugins/functions.php
+++ Simpleplugins/functions.php
@@ -7,8 +7,8 @@
  *
  * @return array
  */
-function csw_setup_menu($menu) {
-	$menu['page_title'] = __('Cart Suggestion Settings');
+function csw_setup_menu( $menu ) {
+	$menu['page_title'] = __( 'Cart Suggestion Settings' );
 	$menu['menu_title'] = $menu['page_title'];
 
 	return $menu;
@@ -45,6 +45,13 @@ function csw_register_settings( $settings ) {
 				'type' => 'text',
 			),
 			array(
+				'id'   => 'cart_message',
+				'name' => __( 'Message to display' ),
+				'desc' => __( 'Variables are [name], [price], [id], [site_url]' ),
+				'type' => 'textarea',
+				'std'  => 'Add our <a href="[site_url]?add-to-cart=[id]">[name]</a> for an extra [price]!'
+			),
+			array(
 				'id'   => 'enable_always',
 				'name' => __( 'Disable same product' ),
 				'desc' => __( 'If you check this option, the message will be displayed even if the user has the product in the cart.' ),
@@ -60,6 +67,7 @@ function csw_register_settings( $settings ) {
 
 	return $settings;
 }
+
 /**
  * Register settings subsections (optional)
  *
@@ -74,11 +82,11 @@ function csw_register_settings_subsections( $subsections ) {
 
 function cart_suggestion_message() {
 	/** @var WC_Product $product */
-	$settings = get_option('csw_settings');
-	$product = wc_get_product( (int) $settings['product_id'] );
-	$display = true;
+	$settings = get_option( 'csw_settings' );
+	$product  = wc_get_product( (int) $settings['product_id'] );
+	$display  = true;
 
-	if (!isset($settings['enable_always'])) {
+	if ( ! isset( $settings['enable_always'] ) ) {
 		foreach ( WC()->cart->get_cart_contents() as $item ) {
 			if ( $item['data']->get_id() == $product->get_id() ) {
 				$display = false;
@@ -88,10 +96,29 @@ function cart_suggestion_message() {
 
 	if ( $display ) {
 
-		echo '<div class="woocommerce-info">Add our <a href="' . site_url() . '/cart?add-to-cart=' . $product->get_id() . '">' .
-		     $product->get_name()
-		     . '</a> for an extra ' .
-		     $product->get_price_html() . '!</div>';
+		$message = csw_get_filtered_message( $product, site_url() );
+
+		echo '<div class="woocommerce-info">' . $message . '</div>';
 	}
 
+}
+
+function filter_cart_message( $output ) {
+	$allowed  = array('a' => array('href' => array ()), 'br' => array(), 'p' => array(), 'strong' => array());
+	return wp_kses( $output, $allowed);
+}
+
+
+function csw_get_filtered_message( WC_Product $product, $url ) {
+
+	$settings = get_option( 'csw_settings' );
+	$message  = $settings['cart_message'];
+
+	// [name], [price], [id], [site_url]
+	$message = str_replace( '[name]', $product->get_name(), $message );
+	$message = str_replace( '[price]', $product->get_price_html(), $message );
+	$message = str_replace( '[id]', $product->get_id(), $message );
+	$message = str_replace( '[site_url]', site_url(), $message );
+
+	return $message;
 }
